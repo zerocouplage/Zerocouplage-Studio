f4e370d76c07db72e745c5f21a3ac56c86eba986
SebbarSara <sara.sebbar.ensao@gmail.com>
2015-05-30 23:51:37 +0100

    preference handler

---
+++ ZCStudio/src/org/zerocouplage/zcstudio/preference/PreferenceConstants.java
@@ -0,0 +1,10 @@
+package org.zerocouplage.zcstudio.preference;
+
+public class PreferenceConstants {
+	public static final  String ZCSDK="ZCSDK_PATH";
+	public static final  String ZCDesktop="JDK_PATH";
+	public static final  String IsClientServeurMobile="IsClientServeur";
+	public static final  String ZCMobile="ZCMobile_PATH";
+	public static final  String IsClientServeurDesktop="IsClientServeur";
+	public static final  String ZCWeb="tomcat_PATH";
+}
