2f4b9829aa6807f1963bf6b42dc309f36eba274f
Douaae <k.douaae@gmail.com>
2015-06-21 17:39:24 +0000

    fin du NewProject

--- ZCStudio/src/org/zerocouplage/zcstudio/newproject/NewZCProjectModel.java
+++ ZCStudio/src/org/zerocouplage/zcstudio/newproject/NewZCProjectModel.java
@@ -5,65 +5,40 @@ import java.io.IOException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-//import org.eclipse.ui.wizards.datatransfer.ImportOperation;
 
-
-
-
-public class NewZCProjectModel
-{
+public class NewZCProjectModel {
 	public static final String copyright = "(c)Copyright 2015 ZCStudio";
-		
-	
 
-
-	
 	// Project Name
 	protected String projectName;
+
 	// New Project Selection
 	protected String selectedExample;
-	
+
 	// choice of JRE
 	protected boolean executionJRE;
-GetProjectLocation cp=new GetProjectLocation();
+	GetProjectLocation locations = new GetProjectLocation();
 
-	// flag is set if a folder called Discounts is selected 
-	// when the wizard is started; a discount is offered in this case
-	boolean discounted = false;	
-//.........................
-//	public String toString()
-//	{
-//	
-//
-//
-//	return selectedExample;
-//			
-//	}
-	//..................................
-//Crean an empty  project 	
-	public void creatProject() throws CoreException, IOException{
+	// Create a new project
+	public void creatProject() throws CoreException, IOException {
 
-		cp.copy(projectName,selectedExample);
-		//Programmatically import the project into Eclipse		
-		
-        IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
-        
-        //if the project already exist it doesn't be create 
-        if (!project.exists()) {
-			
-				project.create(null);
-        
-				project.open(null);
-	                         }			   
-      
+		locations.copyExampleToWorkspace(projectName, selectedExample);
+		// import the project into Eclipse
+
+		IProject project = ResourcesPlugin.getWorkspace().getRoot()
+				.getProject(projectName);
+
+		// if the project already exist it doesn't be create and gives an error
+		// on the log
+		if (!project.exists()) {
+
+			project.create(null);
+		} else {
+			System.out.println("Projet déjà éxistant");
+		}
+
+		project.open(null);
+
 	}
-	
-	
+
 }
-
-			
-	
-	
-
--- ZCStudio/src/org/zerocouplage/zcstudio/newproject/NewZCProjectSettingsPage.java
+++ ZCStudio/src/org/zerocouplage/zcstudio/newproject/NewZCProjectSettingsPage.java
@@ -1,7 +1,6 @@
 package org.zerocouplage.zcstudio.newproject;
 
 import java.io.File;
-import java.io.FileFilter;
 
 import org.eclipse.jface.dialogs.IDialogPage;
 import org.eclipse.jface.wizard.WizardPage;
@@ -14,16 +13,12 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Listener;
 
-
-
-public class NewZCProjectSettingsPage extends WizardPage implements Listener 
-{
+public class NewZCProjectSettingsPage extends WizardPage implements Listener {
 	public static final String copyright = "(c) Copyright 2015 ZCStudio";
-	
+
 	// widgets on this page
 	List exampleProjectList;
 
-		
 	/**
 	 * Constructor for NewZCProjectSettingsPage.
 	 */
@@ -32,121 +27,108 @@ public class NewZCProjectSettingsPage extends WizardPage implements Listener
 		setTitle("Templates");
 		setDescription("Select one of the available templates to generate a fully-functioning ZC project");
 	}
-	
-	
-	//verify if file listed in settingsPage are projects
-	 public boolean verifyZcProject(File project){
-		  
-		          
-	        	   File [] Files =project.listFiles();
-	        	   for(File File : Files){
-	        		   
-	        		   if(File.getName().equals(".project")){
-	        			   return true;
-	        		   }
-	        	   }
-   
-	           
-	    return false;
- }
-	 
-//list content of examples directory
-	 public void listerRepertoire(String pathname){ 
-
-	      // construction d'un fichier sur ce répertoire
-	     File repFile =  new File(pathname) ;
-
-	      // filtrage du contenu de ce répertoire
-	      // on passe en paramètre une instance de classe anonyme
-	     File [] projects = repFile.listFiles() ;
-
-	         // cette interface n'a qu'une unique méthode
-	        
-	        	 for (File project : projects) {
-	        		
-	        		 if(project.isDirectory() && verifyZcProject(project)){
-	        			 String fileName = project.getName();
-	        			 exampleProjectList.add(fileName);
-	        		 }
-	  			   }
-	        		 
-	  			
-
-	        	 
-	         }
-
 
 	/**
 	 * @see IDialogPage#createControl(Composite)
 	 */
 	public void createControl(Composite parent) {
 
-	    // create the composite to hold the widgets
+		// create the composite to hold the widgets
 		GridData gd;
 		Composite composite = new Composite(parent, SWT.NONE);
 
-	    // create the desired layout for this wizard page
+		// create the desired layout for this wizard page
 		GridLayout gl = new GridLayout();
 		int ncol = 2;
 		gl.numColumns = ncol;
 		composite.setLayout(gl);
-	
-	    // flights list
-		Label label = new Label (composite, SWT.NONE);
+
+		// flights list
+		Label label = new Label(composite, SWT.NONE);
 		label.setText("Available Templates :");
 
-		exampleProjectList = new List(composite, SWT.BORDER | SWT.READ_ONLY  );
+		exampleProjectList = new List(composite, SWT.BORDER | SWT.READ_ONLY);
 		gd = new GridData(GridData.FILL_HORIZONTAL);
-		gd.horizontalSpan =ncol;
+		gd.horizontalSpan = ncol;
 		exampleProjectList.setLayoutData(gd);
 		exampleProjectList.addListener(SWT.Selection, this);
 
-		
-	    // set the composite as the control for this page
-		setControl(composite);		
+		// set the composite as the control for this page
+		setControl(composite);
 		setPageComplete(true);
 	}
 
-	public boolean canFlipToNextPage()
-	{
+	// verify if file listed in settingsPage are projects
+	public boolean verifyZcProject(File project) {
+
+		File[] Files = project.listFiles();
+		for (File File : Files) {
+
+			if (File.getName().equals(".project")) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	// list content of examples directory
+	public void listerRepertoire(String pathname) {
+
+		// construction of a file on the directory
+		File repFile = new File(pathname);
+
+		// filter the content of the directory
+		File[] projects = repFile.listFiles();
+
+		// add element to list of example on the wizard
+
+		for (File project : projects) {
+
+			if (project.isDirectory() && verifyZcProject(project)) {
+				String fileName = project.getName();
+				exampleProjectList.add(fileName);
+			}
+		}
+
+	}
+
+	public boolean canFlipToNextPage() {
 		// no next page for this path through the wizard
 		return false;
 	}
-	
-    /*
-     * Process the events: 
-     * when the user has entered all information
-     * the wizard can be finished
-     */
-	public void handleEvent(Event e)
-	{
+
+	/*
+	 * Process the events: when the user has entered all information the wizard
+	 * can be finished
+	 */
+	public void handleEvent(Event e) {
 
 		setPageComplete(isPageComplete());
 		getWizard().getContainer().updateButtons();
 	}
-	
+
 	/*
-	 * Sets the completed field on the wizard class when all the information 
-	 * is entered and the wizard can be completed
-	 */	 
-	public boolean isPageComplete()
-	{
-	
+	 * Sets the completed field on the wizard class when all the information is
+	 * entered and the wizard can be completed
+	 */
+	public boolean isPageComplete() {
+
 		saveDataToModel();
 		return true;
 	}
-	
-	private void saveDataToModel()
-	{
-		NewZCProjectWizard wizard = (NewZCProjectWizard)getWizard();
+
+	// add information to the model
+	private void saveDataToModel() {
+		NewZCProjectWizard wizard = (NewZCProjectWizard) getWizard();
 		wizard.model.selectedExample = exampleProjectList.getSelection()[0];
 		wizard.creationCompleted = true;
-	}	
+	}
 
-	void onEnterPage()
-	{
+	// first method on the wizard
+	void onEnterPage() {
 		exampleProjectList.removeAll();
-		listerRepertoire(GetProjectLocation.ZCSDK+"\\examples");
+		listerRepertoire(GetProjectLocation.ZCSDK + "\\examples");
 
 	}
 }
