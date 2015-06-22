c5507712f4e0dee934c4c911f78385f40f2520f2
SebbarSara <sara.sebbar.ensao@gmail.com>
2015-06-22 02:19:00 +0000

    java doc

--- ZCStudio/src/org/zerocouplage/zcstudio/preference/DesktopPreferencePage.java
+++ ZCStudio/src/org/zerocouplage/zcstudio/preference/DesktopPreferencePage.java
@@ -1,6 +1,5 @@
 package org.zerocouplage.zcstudio.preference;
 
-
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.DirectoryFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
@@ -8,29 +7,36 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.zerocouplage.zcstudio.Activator;
 
- 
+public class DesktopPreferencePage extends FieldEditorPreferencePage implements
+		IWorkbenchPreferencePage {
+	public static final String copyright = "(c)Copyright 2015 ZCStudio";
 
-public class DesktopPreferencePage  extends FieldEditorPreferencePage implements
-IWorkbenchPreferencePage {
+	public DesktopPreferencePage() {
+		super(GRID);
 
-public DesktopPreferencePage () {
-super(GRID);
+	}
 
+	/**
+	 * @see org.eclipse.jface.preference.FieldEditorPreferencePage#createFieldEditors()
+	 *      this method add a field in desktop preference page to enter jdk
+	 *      location
+	 */
+	public void createFieldEditors() {
+		addField(new DirectoryFieldEditor(PreferenceConstants.ZCDesktop,
+				"&JDK Location:", getFieldEditorParent()));
+		/**
+		 * @see org.eclipse.jface.preference.FieldEditorPreferencePage#createFieldEditors()
+		 *      add a field in desktop preference page to tell if it is a
+		 *      client/server app
+		 */
+		addField(new BooleanFieldEditor(
+				PreferenceConstants.IsClientServeurDesktop,
+				"&Application Client/Serveur", getFieldEditorParent()));
+	}
+
+	@Override
+	public void init(IWorkbench workbench) {
+		setPreferenceStore(Activator.getDefault().getPreferenceStore());
+		setDescription("Ajouter la location de votre JDK");
+	}
 }
-
-public void createFieldEditors() {
-addField(new DirectoryFieldEditor(PreferenceConstants.ZCDesktop, "&JDK Location:",
-    getFieldEditorParent()));
-addField(new BooleanFieldEditor(PreferenceConstants.IsClientServeurDesktop,
-        "&Application Client/Serveur", getFieldEditorParent()));
-}
-          
-
-
-
-@Override
-public void init(IWorkbench workbench) {
-setPreferenceStore(Activator.getDefault().getPreferenceStore());
-setDescription("Ajouter la location de votre JDK");
-}
-} 
--- ZCStudio/src/org/zerocouplage/zcstudio/preference/PreferenceConstants.java
+++ ZCStudio/src/org/zerocouplage/zcstudio/preference/PreferenceConstants.java
@@ -1,10 +1,12 @@
 package org.zerocouplage.zcstudio.preference;
 
 public class PreferenceConstants {
-	public static final  String ZCSDK="ZCSDK_PATH";
-	public static final  String ZCDesktop="JDK_PATH";
-	public static final  String IsClientServeurMobile="IsClientServeur";
-	public static final  String ZCMobile="ZCMobile_PATH";
-	public static final  String IsClientServeurDesktop="IsClientServeur";
-	public static final  String ZCWeb="tomcat_PATH";
+	public static final String copyright = "(c)Copyright 2015 ZCStudio";
+
+	public static final String ZCSDK = "ZCSDK_PATH";
+	public static final String ZCDesktop = "JDK_PATH";
+	public static final String IsClientServeurMobile = "IsClientServeur";
+	public static final String ZCMobile = "ZCMobile_PATH";
+	public static final String IsClientServeurDesktop = "IsClientServeur";
+	public static final String ZCWeb = "tomcat_PATH";
 }
